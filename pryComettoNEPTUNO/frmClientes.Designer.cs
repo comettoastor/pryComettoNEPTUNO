24ad9fa6d64fea3c90f57e21e228a2ec8dc82488
root <mgiraudo@ies21.edu.ar>
2023-05-17 13:46:34 -0300

    ofdhafha

--- pryComettoNEPTUNO/frmClientes.Designer.cs
+++ pryComettoNEPTUNO/frmClientes.Designer.cs
@@ -42,8 +42,14 @@
             this.Fax = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnListar = new System.Windows.Forms.Button();
             this.lblListado = new System.Windows.Forms.Label();
-            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.mrcFiltros = new System.Windows.Forms.GroupBox();
+            this.cmbPais = new System.Windows.Forms.ComboBox();
+            this.cmbCiudad = new System.Windows.Forms.ComboBox();
+            this.lblPais = new System.Windows.Forms.Label();
+            this.lblCiudad = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+            this.mrcFiltros.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvClientes
@@ -67,7 +73,7 @@
             this.Pais,
             this.Telefono,
             this.Fax});
-            this.dgvClientes.Location = new System.Drawing.Point(12, 61);
+            this.dgvClientes.Location = new System.Drawing.Point(12, 116);
             this.dgvClientes.Name = "dgvClientes";
             this.dgvClientes.RowHeadersVisible = false;
             this.dgvClientes.Size = new System.Drawing.Size(699, 252);
@@ -111,7 +117,7 @@
             // 
             // Region
             // 
-            this.Region.HeaderText = "Región";
+            this.Region.HeaderText = "Region";
             this.Region.Name = "Region";
             this.Region.Width = 66;
             // 
@@ -146,7 +152,7 @@
             this.btnListar.FlatAppearance.BorderSize = 0;
             this.btnListar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnListar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnListar.Location = new System.Drawing.Point(608, 329);
+            this.btnListar.Location = new System.Drawing.Point(608, 383);
             this.btnListar.Name = "btnListar";
             this.btnListar.Size = new System.Drawing.Size(103, 36);
             this.btnListar.TabIndex = 0;
@@ -164,31 +170,86 @@
             this.lblListado.TabIndex = 2;
             this.lblListado.Text = "Listado de Clientes";
             // 
-            // btnSalir
+            // btnVolver
             // 
-            this.btnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnSalir.Location = new System.Drawing.Point(12, 329);
-            this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(101, 36);
-            this.btnSalir.TabIndex = 6;
-            this.btnSalir.Text = "Volver";
-            this.btnSalir.UseVisualStyleBackColor = false;
-            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            this.btnVolver.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnVolver.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnVolver.Location = new System.Drawing.Point(12, 383);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(101, 36);
+            this.btnVolver.TabIndex = 6;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = false;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // mrcFiltros
+            // 
+            this.mrcFiltros.Controls.Add(this.cmbPais);
+            this.mrcFiltros.Controls.Add(this.cmbCiudad);
+            this.mrcFiltros.Controls.Add(this.lblPais);
+            this.mrcFiltros.Controls.Add(this.lblCiudad);
+            this.mrcFiltros.Location = new System.Drawing.Point(12, 64);
+            this.mrcFiltros.Name = "mrcFiltros";
+            this.mrcFiltros.Size = new System.Drawing.Size(360, 46);
+            this.mrcFiltros.TabIndex = 7;
+            this.mrcFiltros.TabStop = false;
+            this.mrcFiltros.Text = "Filtros";
+            // 
+            // cmbPais
+            // 
+            this.cmbPais.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPais.FormattingEnabled = true;
+            this.cmbPais.Location = new System.Drawing.Point(232, 16);
+            this.cmbPais.Name = "cmbPais";
+            this.cmbPais.Size = new System.Drawing.Size(121, 21);
+            this.cmbPais.TabIndex = 11;
+            this.cmbPais.SelectedIndexChanged += new System.EventHandler(this.cmbPais_SelectedIndexChanged);
+            // 
+            // cmbCiudad
+            // 
+            this.cmbCiudad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCiudad.FormattingEnabled = true;
+            this.cmbCiudad.Location = new System.Drawing.Point(55, 16);
+            this.cmbCiudad.Name = "cmbCiudad";
+            this.cmbCiudad.Size = new System.Drawing.Size(121, 21);
+            this.cmbCiudad.TabIndex = 10;
+            this.cmbCiudad.SelectedIndexChanged += new System.EventHandler(this.cmbCiudad_SelectedIndexChanged);
+            // 
+            // lblPais
+            // 
+            this.lblPais.AutoSize = true;
+            this.lblPais.Location = new System.Drawing.Point(194, 19);
+            this.lblPais.Name = "lblPais";
+            this.lblPais.Size = new System.Drawing.Size(32, 13);
+            this.lblPais.TabIndex = 9;
+            this.lblPais.Text = "País:";
+            // 
+            // lblCiudad
+            // 
+            this.lblCiudad.AutoSize = true;
+            this.lblCiudad.Location = new System.Drawing.Point(6, 19);
+            this.lblCiudad.Name = "lblCiudad";
+            this.lblCiudad.Size = new System.Drawing.Size(43, 13);
+            this.lblCiudad.TabIndex = 8;
+            this.lblCiudad.Text = "Ciudad:";
             // 
             // frmClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(721, 375);
-            this.Controls.Add(this.btnSalir);
+            this.ClientSize = new System.Drawing.Size(721, 431);
+            this.Controls.Add(this.mrcFiltros);
+            this.Controls.Add(this.btnVolver);
             this.Controls.Add(this.lblListado);
             this.Controls.Add(this.btnListar);
             this.Controls.Add(this.dgvClientes);
             this.Name = "frmClientes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Listado de Clientes - NEPTUNO";
+            this.Load += new System.EventHandler(this.frmClientes_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+            this.mrcFiltros.ResumeLayout(false);
+            this.mrcFiltros.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,6 +258,14 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgvClientes;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Label lblListado;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.GroupBox mrcFiltros;
+        private System.Windows.Forms.ComboBox cmbPais;
+        private System.Windows.Forms.ComboBox cmbCiudad;
+        private System.Windows.Forms.Label lblPais;
+        private System.Windows.Forms.Label lblCiudad;
         private System.Windows.Forms.DataGridViewTextBoxColumn idCliente;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nombre_Compania;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nombre_Contacto;
@@ -208,8 +277,5 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Pais;
         private System.Windows.Forms.DataGridViewTextBoxColumn Telefono;
         private System.Windows.Forms.DataGridViewTextBoxColumn Fax;
-        private System.Windows.Forms.Button btnListar;
-        private System.Windows.Forms.Label lblListado;
-        private System.Windows.Forms.Button btnSalir;
     }
 }
--- pryComettoNEPTUNO/frmClientes.cs
+++ pryComettoNEPTUNO/frmClientes.cs
@@ -13,20 +13,15 @@ namespace pryComettoNEPTUNO
 {
     public partial class frmClientes : Form
     {
-        public frmClientes()
-        {
-            InitializeComponent();
-        }
+        OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=NEPTUNO.accdb;Persist Security Info=False;");
+        OleDbCommand comando = new OleDbCommand();
+        OleDbDataReader lector;
 
-        private void btnListar_Click(object sender, EventArgs e)
+        public void ListarClientes()
         {
             dgvClientes.Rows.Clear();
             try
             {
-                OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=NEPTUNO.accdb;Persist Security Info=False;");
-                OleDbCommand comando = new OleDbCommand();
-                OleDbDataReader lector;
-
                 comando.Connection = conexion;
                 comando.CommandText = "Clientes";
                 comando.CommandType = CommandType.TableDirect;
@@ -37,6 +32,7 @@ namespace pryComettoNEPTUNO
                 while (lector.Read())
                 {
                     dgvClientes.Rows.Add(lector[0], lector[1], lector[2], lector[3], lector[4], lector[5], lector[6], lector[7], lector[8], lector[9], lector[10]);
+                    cmbCiudad.Items.Add(lector[5]);
                 }
 
                 comando.Connection.Close();
@@ -47,12 +43,146 @@ namespace pryComettoNEPTUNO
             }
         }
 
-        private void btnSalir_Click(object sender, EventArgs e)
+        public void Volver()
         {
             frmMenu frmMenu = new frmMenu();
             this.Hide();
             frmMenu.ShowDialog();
             this.Close();
+
+        }
+
+        public void CargarPaisCiudad()
+        {
+            bool encontradoCiudad = false;
+            bool encontradoPais = false;
+            try
+            {
+                comando.Connection = conexion;
+                comando.CommandText = "Clientes";
+                comando.CommandType = CommandType.TableDirect;
+                comando.Connection.Open();
+
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    for (int i = 0; i < cmbCiudad.Items.Count; i++)
+                    {
+                        if (lector[5].ToString() == cmbCiudad.Items[i].ToString())
+                        {
+                            encontradoCiudad = true;
+                        }
+                    }
+                    if (encontradoCiudad == false)
+                    {
+                        cmbCiudad.Items.Add(lector[5]);
+                    }
+                    encontradoCiudad = false;
+
+                    for (int i = 0; i < cmbPais.Items.Count; i++)
+                    {
+                        if (lector[8].ToString() == cmbPais.Items[i].ToString())
+                        {
+                            encontradoPais = true;
+                        }
+                    }
+                    if (encontradoPais == false)
+                    {
+                        cmbPais.Items.Add(lector[8]);
+                    }
+                    encontradoPais = false;
+                }
+                comando.Connection.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        public void ListarPais()
+        {
+            dgvClientes.Rows.Clear();
+            try
+            {
+                comando.Connection = conexion;
+                comando.CommandText = "Clientes";
+                comando.CommandType = CommandType.TableDirect;
+                comando.Connection.Open();
+
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    if (lector[8].ToString() == cmbPais.Text)
+                    {
+                        dgvClientes.Rows.Add(lector[0], lector[1], lector[2], lector[3], lector[4], lector[5], lector[6], lector[7], lector[8], lector[9], lector[10]);
+                    }
+                }
+                comando.Connection.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        public void ListarCiudad()
+        {
+            dgvClientes.Rows.Clear();
+            try
+            {
+                comando.Connection = conexion;
+                comando.CommandText = "Clientes";
+                comando.CommandType = CommandType.TableDirect;
+                comando.Connection.Open();
+
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    if (lector[5].ToString() == cmbCiudad.Text)
+                    {
+                        dgvClientes.Rows.Add(lector[0], lector[1], lector[2], lector[3], lector[4], lector[5], lector[6], lector[7], lector[8], lector[9], lector[10]);
+                    }
+                }
+                comando.Connection.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        public frmClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            ListarClientes();
+        }
+
+        private void frmClientes_Load(object sender, EventArgs e)
+        {
+            CargarPaisCiudad();
+        }
+
+        private void cmbPais_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListarPais();
+        }
+
+        private void cmbCiudad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListarCiudad();
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Volver();
         }
     }
 }
