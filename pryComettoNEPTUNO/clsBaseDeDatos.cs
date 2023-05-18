4af0271d8d4d32e4fadb2f25f587be0acbc87605
root <mgiraudo@ies21.edu.ar>
2023-05-18 14:02:53 -0300

    dgjj

---
+++ pryComettoNEPTUNO/clsBaseDeDatos.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+using System.Windows.Forms;
+
+namespace pryComettoNEPTUNO
+{
+    internal class clsBaseDeDatos
+    {
+        public OleDbCommand Comando = new OleDbCommand();
+        public OleDbDataReader Lector;
+
+        public void ListarClientes(DataGridView dgvClientes, string BaseDeDatos, string Tabla)
+        {
+            OleDbConnection Conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + BaseDeDatos + ".accdb;Persist Security Info=False;");
+            dgvClientes.Rows.Clear();
+            try
+            {
+                Comando.Connection = Conexion;
+                Comando.CommandText = Tabla;
+                Comando.CommandType = CommandType.TableDirect;
+                Comando.Connection.Open();
+
+                Lector = Comando.ExecuteReader();
+
+                while (Lector.Read())
+                {
+                    dgvClientes.Rows.Add(Lector[0], Lector[1], Lector[2], Lector[3], Lector[4], Lector[5], Lector[6], Lector[7], Lector[8], Lector[9], Lector[10]);
+                }
+
+                Comando.Connection.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        public void CargarPaisCiudad(ComboBox cmbCiudad, ComboBox cmbPais, string BaseDeDatos, string Tabla)
+        {
+            OleDbConnection Conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + BaseDeDatos + ".accdb;Persist Security Info=False;");
+            bool encontradoCiudad = false;
+            bool encontradoPais = false;
+            try
+            {
+                Comando.Connection = Conexion;
+                Comando.CommandText = Tabla;
+                Comando.CommandType = CommandType.TableDirect;
+                Comando.Connection.Open();
+
+                Lector = Comando.ExecuteReader();
+
+                while (Lector.Read())
+                {
+                    for (int i = 0; i < cmbCiudad.Items.Count; i++)
+                    {
+                        if (Lector[5].ToString() == cmbCiudad.Items[i].ToString())
+                        {
+                            encontradoCiudad = true;
+                        }
+                    }
+                    if (encontradoCiudad == false)
+                    {
+                        cmbCiudad.Items.Add(Lector[5]);
+                    }
+                    encontradoCiudad = false;
+
+                    for (int i = 0; i < cmbPais.Items.Count; i++)
+                    {
+                        if (Lector[8].ToString() == cmbPais.Items[i].ToString())
+                        {
+                            encontradoPais = true;
+                        }
+                    }
+                    if (encontradoPais == false)
+                    {
+                        cmbPais.Items.Add(Lector[8]);
+                    }
+                    encontradoPais = false;
+                }
+                Comando.Connection.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        public void ListarPais(DataGridView dgvClientes, string BaseDeDatos, string Tabla, ComboBox cmbPais)
+        {
+            OleDbConnection Conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + BaseDeDatos + ".accdb;Persist Security Info=False;");
+            dgvClientes.Rows.Clear();
+            if (cmbPais.SelectedIndex != -1)
+            {
+                try
+                {
+                    Comando.Connection = Conexion;
+                    Comando.CommandText = Tabla;
+                    Comando.CommandType = CommandType.TableDirect;
+                    Comando.Connection.Open();
+
+                    Lector = Comando.ExecuteReader();
+
+                    while (Lector.Read())
+                    {
+                        if (Lector[8].ToString() == cmbPais.Text)
+                        {
+                            dgvClientes.Rows.Add(Lector[0], Lector[1], Lector[2], Lector[3], Lector[4], Lector[5], Lector[6], Lector[7], Lector[8], Lector[9], Lector[10]);
+                        }
+                    }
+                    Comando.Connection.Close();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+        }
+
+        public void ListarCiudad(DataGridView dgvClientes, string BaseDeDatos, string Tabla, ComboBox cmbCiudad)
+        {
+            OleDbConnection Conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + BaseDeDatos + ".accdb;Persist Security Info=False;");
+            dgvClientes.Rows.Clear();
+            try
+            {
+                Comando.Connection = Conexion;
+                Comando.CommandText = Tabla;
+                Comando.CommandType = CommandType.TableDirect;
+                Comando.Connection.Open();
+
+                Lector = Comando.ExecuteReader();
+
+                while (Lector.Read())
+                {
+                    if (Lector[5].ToString() == cmbCiudad.Text)
+                    {
+                        dgvClientes.Rows.Add(Lector[0], Lector[1], Lector[2], Lector[3], Lector[4], Lector[5], Lector[6], Lector[7], Lector[8], Lector[9], Lector[10]);
+                    }
+                }
+                Comando.Connection.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+    }
+}
--- pryComettoNEPTUNO/frmClientes.cs
+++ pryComettoNEPTUNO/frmClientes.cs
@@ -13,148 +13,6 @@ namespace pryComettoNEPTUNO
 {
     public partial class frmClientes : Form
     {
-        OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=NEPTUNO.accdb;Persist Security Info=False;");
-        OleDbCommand comando = new OleDbCommand();
-        OleDbDataReader lector;
-
-        public void ListarClientes()
-        {
-            dgvClientes.Rows.Clear();
-            try
-            {
-                comando.Connection = conexion;
-                comando.CommandText = "Clientes";
-                comando.CommandType = CommandType.TableDirect;
-                comando.Connection.Open();
-
-                lector = comando.ExecuteReader();
-
-                while (lector.Read())
-                {
-                    dgvClientes.Rows.Add(lector[0], lector[1], lector[2], lector[3], lector[4], lector[5], lector[6], lector[7], lector[8], lector[9], lector[10]);
-                    cmbCiudad.Items.Add(lector[5]);
-                }
-
-                comando.Connection.Close();
-            }
-            catch (Exception error)
-            {
-                MessageBox.Show(error.Message);
-            }
-        }
-
-        public void Volver()
-        {
-            frmMenu frmMenu = new frmMenu();
-            this.Hide();
-            frmMenu.ShowDialog();
-            this.Close();
-
-        }
-
-        public void CargarPaisCiudad()
-        {
-            bool encontradoCiudad = false;
-            bool encontradoPais = false;
-            try
-            {
-                comando.Connection = conexion;
-                comando.CommandText = "Clientes";
-                comando.CommandType = CommandType.TableDirect;
-                comando.Connection.Open();
-
-                lector = comando.ExecuteReader();
-
-                while (lector.Read())
-                {
-                    for (int i = 0; i < cmbCiudad.Items.Count; i++)
-                    {
-                        if (lector[5].ToString() == cmbCiudad.Items[i].ToString())
-                        {
-                            encontradoCiudad = true;
-                        }
-                    }
-                    if (encontradoCiudad == false)
-                    {
-                        cmbCiudad.Items.Add(lector[5]);
-                    }
-                    encontradoCiudad = false;
-
-                    for (int i = 0; i < cmbPais.Items.Count; i++)
-                    {
-                        if (lector[8].ToString() == cmbPais.Items[i].ToString())
-                        {
-                            encontradoPais = true;
-                        }
-                    }
-                    if (encontradoPais == false)
-                    {
-                        cmbPais.Items.Add(lector[8]);
-                    }
-                    encontradoPais = false;
-                }
-                comando.Connection.Close();
-            }
-            catch (Exception error)
-            {
-                MessageBox.Show(error.Message);
-            }
-        }
-
-        public void ListarPais()
-        {
-            dgvClientes.Rows.Clear();
-            try
-            {
-                comando.Connection = conexion;
-                comando.CommandText = "Clientes";
-                comando.CommandType = CommandType.TableDirect;
-                comando.Connection.Open();
-
-                lector = comando.ExecuteReader();
-
-                while (lector.Read())
-                {
-                    if (lector[8].ToString() == cmbPais.Text)
-                    {
-                        dgvClientes.Rows.Add(lector[0], lector[1], lector[2], lector[3], lector[4], lector[5], lector[6], lector[7], lector[8], lector[9], lector[10]);
-                    }
-                }
-                comando.Connection.Close();
-            }
-            catch (Exception error)
-            {
-                MessageBox.Show(error.Message);
-            }
-        }
-
-        public void ListarCiudad()
-        {
-            dgvClientes.Rows.Clear();
-            try
-            {
-                comando.Connection = conexion;
-                comando.CommandText = "Clientes";
-                comando.CommandType = CommandType.TableDirect;
-                comando.Connection.Open();
-
-                lector = comando.ExecuteReader();
-
-                while (lector.Read())
-                {
-                    if (lector[5].ToString() == cmbCiudad.Text)
-                    {
-                        dgvClientes.Rows.Add(lector[0], lector[1], lector[2], lector[3], lector[4], lector[5], lector[6], lector[7], lector[8], lector[9], lector[10]);
-                    }
-                }
-                comando.Connection.Close();
-            }
-            catch (Exception error)
-            {
-                MessageBox.Show(error.Message);
-            }
-        }
-
         public frmClientes()
         {
             InitializeComponent();
@@ -162,27 +20,34 @@ namespace pryComettoNEPTUNO
 
         private void btnListar_Click(object sender, EventArgs e)
         {
-            ListarClientes();
+            clsBaseDeDatos clsBaseDeDatos = new clsBaseDeDatos();
+            clsBaseDeDatos.ListarClientes(dgvClientes,"NEPTUNO","Clientes");
         }
 
         private void frmClientes_Load(object sender, EventArgs e)
         {
-            CargarPaisCiudad();
+            clsBaseDeDatos clsBaseDeDatos = new clsBaseDeDatos();
+            clsBaseDeDatos.CargarPaisCiudad(cmbCiudad, cmbPais, "NEPTUNO", "Clientes");
         }
 
         private void cmbPais_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ListarPais();
+            clsBaseDeDatos clsBaseDeDatos = new clsBaseDeDatos();
+            clsBaseDeDatos.ListarPais(dgvClientes, "NEPTUNO", "Clientes", cmbPais);
         }
 
         private void cmbCiudad_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ListarCiudad();
+            clsBaseDeDatos clsBaseDeDatos = new clsBaseDeDatos();
+            clsBaseDeDatos.ListarCiudad(dgvClientes, "NEPTUNO", "Clientes", cmbCiudad);
         }
 
         private void btnVolver_Click(object sender, EventArgs e)
         {
-            Volver();
+            frmMenu frmMenu = new frmMenu();
+            this.Hide();
+            frmMenu.ShowDialog();
+            this.Close();
         }
     }
 }
